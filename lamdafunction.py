cf47316ae64d14ac5513d75c0a44a3d1cfba777d
moseti1 <mosetiderrick1@gmail.com>
2022-01-06 14:06:14 +0300

    added images and writeup

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_Name='pytorch-inference-2022-01-05-11-35-51-593'
 
 def lambda_handler(event, context):
 
